7f258989c872aaa92335a662874572d284249f6b
tiennma2k23cvp <tiennma2k23@cvp.vn>
2021-10-16 12:24:19 +0700

    addfile

---
+++ HDT-JSB26/lab_day4/index.js
@@ -0,0 +1,20 @@
+alert("Bài 1");
+let firstName=prompt("First Name :");
+let surName=prompt("SurName :");
+let lastName=prompt("Last Name :");
+
+console.log("Your name is: " + firstName+" "+surName+" "+lastName);
+alert("Your name is: " + firstName+" "+surName+" "+lastName);
+
+alert("Bài 2");
+let num1=Number (prompt("Number 1: "));
+let num2=Number (prompt("Number 2: "));
+let ans=num1%num2;
+console.log("Số dư của "+String(num1)+" khi chia cho "+String(num2)+" là: "+String(ans));
+alert("Số dư của "+String(num1)+" khi chia cho "+String(num2)+" là: "+String(ans));
+
+alert("Bài 3");
+let st=prompt("String input: ");
+let len=Number(st.length);
+console.log("Độ dài xâu đã nhập là: "+String(len));
+alert("Độ dài xâu đã nhập là: "+String(len));
