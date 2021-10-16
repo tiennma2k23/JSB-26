7468d5b577210c0340699a0a40d1cb5b4f96927e
tiennma2k23cvp <tiennma2k23@cvp.vn>
2021-10-16 14:34:59 +0700

    add file

---
+++ HDT-JSB26/day5/index.js
@@ -0,0 +1,15 @@
+let female=Number(prompt("Female Student: "));
+let male=Number(prompt("Male Student: "));
+let sum=female+male;
+if (sum>100)
+{
+    alert("Quá nhiều học sinh");
+}
+else if (sum>=30)
+{
+    alert("Đủ học sinh");
+}
+else 
+{
+    alert("Ít học sinh");
+}
